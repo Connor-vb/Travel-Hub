d19d05f2e95c51edaa8aed10f534d1233b3644f2
Monika Kothari <mkothari.1993@gmail.com>
2021-10-25 23:32:16 -0500

    updating first api

--- script.js
+++ script.js
@@ -1,55 +1,13 @@
-
-fetch("https://google-flights-search.p.rapidapi.com/search?departure_airport_code=SFO&arrival_airport_code=LAX&flight_class=Economy", {
+fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
 	"method": "GET",
 	"headers": {
-		"x-rapidapi-host": "google-flights-search.p.rapidapi.com",
+		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
 		"x-rapidapi-key": "bb3ea38cffmsh4809167876e83cbp105b8fjsn30ad018ac012"
     }
 })
-.then(response => {
-    return response.json();
-})
-.then(function (data) {
-  console.log(data)
-})
-.catch(err => {
-	console.error(err);
+.then((response) => {return response.json()})
+.then(function(flightResponse){
+    var pEl = document.createElement('p');
+    pEl.textContent = flightResponse.Places[0].CountryName;
+    document.getElementById("arrival-airport").appendChild(pEl);
 });
-
-
-
-
-
-
-
-// fetch("https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/autosuggest/v1.0/UK/GBP/en-GB/?query=Stockholm", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-// 		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
-
-
-
-
-
-
-
-
-// return response.json();
-//   })
-  
-// })
-
-//   for (var i = 0; i < data.length; i++) {
-//     console.log(data[i].url);
-//     console.log(data[i].user.login);
-//   }
-// });
