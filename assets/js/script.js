d699bcda4d8e53af14eaa39fcf6eb0bcaa2c1f48
Monika Kothari <mkothari.1993@gmail.com>
2021-11-01 23:34:15 -0500

    updated flight-type input and added max price input

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ $("#search-flight").click(function(event){
 
 
 $("#search-flight").click(function (){
+   var flightType = $("#flight-type").val();
    var depCity =  $("#dep-from").val();
    var departureCity = depCity.toUpperCase();
    var arriveCity = $("#dep-to").val();
@@ -28,10 +29,41 @@ $("#search-flight").click(function (){
    var departureDate = $("#leave-date").val();
    var returnDate = $("#return-date").val();
    var classType = $("#class-type").val();
+   var maxPrice = $("#price-range").val();
    var passengerNo = $("#adults").val();
-      console.log (departureCity, arrivalCity, departureDate, returnDate,classType, passengerNo)
+      console.log (departureCity, arrivalCity, departureDate, returnDate,classType, maxPrice, passengerNo)
+
+   fetch(`https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT&keyword=${departureCity}&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL`,
+      {
+         "method": "GET",
+         "headers": {
+               "Authorization": "Bearer T0rry623cYG5bPi7Yd2VSGQiPEH3",
+         }
+      })
+      .then((response) => {
+         return response.json()
+      })
+      .then(function(departureResponse){
+         var departureCityCode = departureResponse.data[0].iataCode
+      
+
+      fetch(`https://test.api.amadeus.com/v1/reference-data/locations?subType=AIRPORT&keyword=${arrivalCity}&page%5Blimit%5D=10&page%5Boffset%5D=0&sort=analytics.travelers.score&view=FULL`,
+      {
+         "method": "GET",
+         "headers": {
+               "Authorization": "Bearer T0rry623cYG5bPi7Yd2VSGQiPEH3",
+         }
+      })
+      .then((response) => {
+         return response.json()
+      })
+      .then(function(arrivalResponse){
+         var arrivalCityCode = arrivalResponse.data[0].iataCode;
+      
+
    var appendCard= `<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="..."><div class="card-body"><h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>`
-   var flightResponse = fetch(`https://priceline-com-provider.p.rapidapi.com/v1/flights/search?sort_order=PRICE&location_departure=${departureCity}&date_departure=${departureDate}&class_type=${classType}&location_arrival=${arrivalCity}&itinerary_type=ROUND_TRIP&date_departure_return=${returnDate}&number_of_passengers=${passengerNo}`,
+   
+   fetch(`https://priceline-com-provider.p.rapidapi.com/v1/flights/search?sort_order=PRICE&location_departure=${departureCityCode}&date_departure=${departureDate}&class_type=${classType}&location_arrival=${arrivalCityCode}&itinerary_type=${flightType}&date_departure_return=${returnDate}&number_of_passengers=${passengerNo}&price_max=${maxPrice}&number_of_stops=0`,
       {
          "method": "GET",
          "headers": {
@@ -40,28 +72,21 @@ $("#search-flight").click(function (){
          }
       })
       .then((response) => {return response.json()})
-      .then(function(flightResponse){
-         console.log(flightResponse)  
-         console.log(flightResponse.Carriers)  
-         $("#carrier").append('<li>'+JSON.stringify(flightResponse.Carriers[0].Name)+'</li>');
-         $("#carrier").append(`<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="..."><div class="card-body">${flightResponse.Carriers[0].Name}<h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>`);
+      .then(function(providerCoverage){
+         //console.log(providerCoverage)  
+         console.log(providerCoverage)
+         for (i=0; i < 6; i++){
+            console.log(providerCoverage.airline[i].name)
+         }
+         // $("#carrier").append('<li>'+JSON.stringify(providerCoverage.airline[0].name)+'</li>');
+         // $("#carrier").append(`<div class="card" style="width: 18rem;"><img src="..." class="card-img-top" alt="..."><div class="card-body">${providerCoverage.airline[0].name}<h5 class="card-title">Card title</h5><p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p><a href="#" class="btn btn-primary">Go somewhere</a></div></div>`);
       })
       .catch(err => {
          alert(err);
-      });});
-
-
-   
-// var covidURL = `https://test.api.amadeus.com/v1/duty-of-care/diseases/covid19-area-report?countryCode=US${covidKey}`
-// var covidKey = `f3PZijAuvuoT9k2Q6YkNbyY7lZI0U`
-// fetch(covidURL).then(function (response) {
-//    if (response.ok) {
-//      response.json().then(function () {
-//        console.log("did it", response);
-     
-//      });
-//    }
-//  });
+      });
+   });
+   });
+});
 
 
 
